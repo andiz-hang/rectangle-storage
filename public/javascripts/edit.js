d3fdd213a0a64c77d4a9def29f5c215723dc852d
andiz <andiz@sfu.ca>
2021-07-23 16:22:36 -0700

    readded the cancel button to the edit form

--- public/javascripts/edit.js
+++ public/javascripts/edit.js
@@ -5,9 +5,12 @@ function editRect() {
         url: '/rectangle/edit',
         data: 'id='+$('#id').val()+'&width='+$('#width').val()+'&height='+$('#height').val()+'&color='+$('#color').val(),
         success: function(data) {
-            window.self.close();
             window.close();
-            self.close();
         }
     });
+};
+
+// Cancel Edit
+function cancelEdit() {
+    window.close();
 };
